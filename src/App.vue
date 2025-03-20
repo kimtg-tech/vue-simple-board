77f89daacb85fe708a9fd9d6c781b34367463040
tgkim <ktgstar@gmail.com>
2025-03-20 23:25:15 +0900

    수정

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-    <div>안녕 tgkim?</div>
+    <div>안녕 tgkim? 잘부탁해해</div>
     <!-- <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
